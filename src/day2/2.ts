8438c236bfe8b1de861bb0b186818dce8f9a1d18
doubledebug <duledodicc@gmail.com>
2025-12-09 16:36:34 +0100

    day 2

---
+++ src/day2/2.ts
@@ -0,0 +1,41 @@
+import { readInputFromFile } from "../utils/fs.ts";
+
+function isIdInvalid(id: string): boolean {
+  for (let seqLen = 1; seqLen <= Math.floor(id.length / 2); seqLen++) {
+    if (id.length % seqLen !== 0) continue;
+
+    const numOfSeqOccurances = id.length / seqLen;
+    const sequence = id.slice(0, seqLen);
+    const repeatedSequence = Array(numOfSeqOccurances).fill(sequence).join("");
+    if (id === repeatedSequence) return true;
+  }
+
+  return false;
+}
+
+function getSumOfInvalidIds(ranges: string[]): number {
+  let sum = 0;
+
+  for (let i = 0; i < ranges.length; i++) {
+    const range = ranges[i];
+    const [start, end] = range.split("-");
+    const startNum = Number(start);
+    const endNum = Number(end);
+
+    const invalidIds: number[] = [];
+    for (let j = startNum; j <= endNum; j++) {
+      const currentId = String(j);
+      const isValid = isIdInvalid(currentId);
+      if (isValid) invalidIds.push(j);
+    }
+
+    sum += invalidIds.reduce((idSum, id) => idSum + id, 0);
+  }
+
+  return sum;
+}
+
+const rangesString = readInputFromFile("src/day2/input");
+const listOfRanges = rangesString[0].split(",");
+const sumOfInvalidIds = getSumOfInvalidIds(listOfRanges);
+console.log("Sum of invalid IDs is", sumOfInvalidIds);
